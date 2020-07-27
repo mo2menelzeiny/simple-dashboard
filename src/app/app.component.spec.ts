3a635605a7395a309a5cc207f12677e1998dc2de
mo2menelzeiny <mo2menelzeiny@gmail.com>
2020-07-27 14:52:43 +0200

    app component shallow test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { MatGridList } from '@angular/material/grid-list';
-import { RouterTestingModule } from '@angular/router/testing';
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent,
-        MatGridList
-      ],
+      declarations: [AppComponent],
       imports: [RouterTestingModule]
     }).compileComponents();
   }));
@@ -20,17 +17,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'simple-dashboard'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('simple-dashboard');
-  });
-
-  it('should render title', () => {
+  it('should render router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    console.log(compiled.querySelector('h1').textContent);
-    expect(compiled.querySelector('h1').textContent).toContain('SIMPLE DASHBOARD');
+    const element = compiled.querySelector('router-outlet');
+    expect(element).toBeTruthy();
   });
 });
