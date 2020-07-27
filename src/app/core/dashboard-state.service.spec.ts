c38787a0d7d1fa2dd7ff8d166b23e6bcdf1320bc
mo2menelzeiny <mo2menelzeiny@gmail.com>
2020-07-27 17:10:06 +0200

    test: dashboard state

--- src/app/core/dashboard-state.service.spec.ts
+++ src/app/core/dashboard-state.service.spec.ts
@@ -1,6 +1,8 @@
 import {TestBed} from '@angular/core/testing';
+import {take} from 'rxjs/operators';
 
 import {DashboardStateService} from './dashboard-state.service';
+import {Customer} from '../models/customer';
 
 describe('DashboardStateService', () => {
   let service: DashboardStateService;
@@ -12,5 +14,14 @@ describe('DashboardStateService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should modify customer state', async () => {
+    const customers: Customer[] = [
+      {firstName: 'test', lastName: 'test', email: 'test@test.com', country: 'test', avatar: 'test'}
+    ];
+    service.setCustomers(customers);
+    const expected = await service.getCustomers$().pipe(take(1)).toPromise();
+    expect(expected).toEqual(customers);
   });
 });
