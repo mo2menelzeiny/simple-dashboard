d458ffe04f6bfadb44c83e4974469cccd5dcf75b
mo2menelzeiny <mo2menelzeiny@gmail.com>
2020-07-27 18:10:28 +0200

    test: customer api

--- src/app/core/customer-api.service.spec.ts
+++ src/app/core/customer-api.service.spec.ts
@@ -1,16 +1,32 @@
 import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 import {CustomerApiService} from './customer-api.service';
+import {CustomerDto} from '../models/customer-dto';
 
 describe('DashboardApiService', () => {
   let service: CustomerApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(CustomerApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should successfully get customers', () => {
+    const customers: CustomerDto[] = [
+      {id: 1, first_name: 'test', last_name: 'test', email: 'test@test.com', country: 'test', avatar: 'test'}
+    ];
+    const httpTestingController = TestBed.inject(HttpTestingController);
+    service.loadCustomers().subscribe((expected) => expect(customers).toEqual(expected));
+    const mockRequest = httpTestingController.expectOne(service.API);
+    expect(mockRequest.request.method).toBe('GET');
+    mockRequest.flush(customers);
+    httpTestingController.verify();
   });
 });
